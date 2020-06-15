c2bb7e44381743933958b7e1a667b43158d17ce0
Fall-man <a-takeuchi@trinome-design.com>
2020-06-15 18:19:37 +0900

    ADD minimize file structures.

---
+++ js/src.js
@@ -0,0 +1,60 @@
+// import {WEBGL} from './three/examples/jsm/WebGL.js';
+
+var scene, camera, renderer;
+var geometry, material, mesh;
+var canvas;
+
+init();
+render();
+
+// 初期化
+function init(){
+
+    // if(WebGL.isWebGL2Available() === false){
+    //     document.body.appendChild(WEBGL.getWebGL2ErrorMessage());
+    // }
+
+    // シーンとカメラオブジェクトを作成
+    scene = new THREE.Scene();
+    camera = new THREE.Camera();
+
+    //　何もないBuffer Geometry を作成
+    geometry = new THREE.PlaneBufferGeometry(2.0, 2.0);
+
+    // Shaderをマテリアルに当てる，Uniformとシェーダファイルの紐付け
+    material = new THREE.ShaderMaterial({
+        uniforms: {
+            time: { type:   "f", value: 0.0 },
+            resolution: {   type:   "v2",   value:  new THREE.Vector2(1024.0, 720.0) }
+        },
+        vertexShader: document.getElementById('vs').textContent,
+        fragmentShader: document.getElementById('fs').textContent
+    });
+
+    // meshを作成し，シーンに紐づける
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    // rendererを512x512のサイズで作成
+    // var canvas_GL2 = document.createElement('canvas');
+    // var context = canvas.getContext('webgl2', {alpha: false});
+    renderer = new THREE.WebGLRenderer(
+    // {
+        // canvas: canvas, 
+        // context: context
+    // }
+    );
+    renderer.setSize(1024.0, 720.0);
+
+
+    // canvasを作成し，div要素に紐付ける
+    canvas = renderer.domElement;
+    document.getElementById('containar').appendChild(canvas);
+}
+
+// ループ
+function render(timestamp){
+    requestAnimationFrame(render);
+    material.uniforms.time.value = timestamp * 0.001;
+    renderer.render(scene, camera);
+}
